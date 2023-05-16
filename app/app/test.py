2336075f655b84a076334bb5988c35f587b601b3
Parv Dave <parvdave@Parvs-MacBook-Air.local>
2023-05-17 00:42:42 +0530

    added postgres

---
+++ app/app/test.py
@@ -0,0 +1,11 @@
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add(self):
+        x = 5
+        y = 10
+
+        self.assertEqual(calc.add(x,y),15)
